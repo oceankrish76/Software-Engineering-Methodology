32dcb5e48009458dae8c57c1285fb96d83d69148
Arno <arno.lehtonen@gmail.com>
2017-02-04 21:24:54 +0200

    Test player model, level collision, a dummy pickup.

---
+++ code/River Raid 2017/Assets/Scripts/Level/LevelColliderBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelColliderBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            Destroy(other.gameObject);
+            Debug.Log("GameOver");
+        }
+    }
+}
---
+++ code/River Raid 2017/Assets/Scripts/Player/PlayerInput.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour {
+
+    public float moveSpeed = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+
+    void FixedUpdate () {
+
+        if(Input.GetButton("Horizontal"))
+        {
+            transform.localPosition += Input.GetAxis("Horizontal") * transform.right * Time.deltaTime * moveSpeed;
+        }
+        
+        if(Input.GetButton("Vertical"))
+        {
+            transform.localPosition += Input.GetAxis("Vertical") * transform.forward * Time.deltaTime * moveSpeed;
+        }
+	}
+}
