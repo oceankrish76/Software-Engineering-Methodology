0e9525fb48da9225aaa63fa1b79239fdbe901bf6
Arno <arno.lehtonen@gmail.com>
2017-02-23 17:37:28 +0200

    UI, pickup functionality enhancements.2

---
+++ code/River Raid 2017/Assets/Scripts/Pickups/Pickup.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+
+
+    public enum PickupTypes
+    {
+        Pickup_Fuel,
+        Pickup_Weapon,
+        Pickup_Other
+    };
+
+    public PickupTypes pickupType;
+    public GameObject explosion;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("Pickup");
+
+            switch (pickupType)
+            {
+                case PickupTypes.Pickup_Fuel:
+
+                    break;
+                case PickupTypes.Pickup_Weapon:
+
+                    Destroy(gameObject);
+                    break;
+                case PickupTypes.Pickup_Other:
+
+                    Destroy(gameObject);
+                    break;
+                default:
+                    Debug.Log("whatsthis");
+                    break;
+            }
+
+            
+        }
+
+        if (other.gameObject.tag == "Bullets")
+        {
+            GameObject particles = Instantiate(explosion, transform.position, transform.rotation);
+
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
+    }
+
+
+
+}
