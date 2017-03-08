a8a399201e945714761da9226ccd6b3824d43769
Rekalibrovka <kairbek.kuanysh@gmail.com>
2017-03-08 03:20:54 +0200

    =River and banks + collider

---
+++ code/River Raid 2017/Assets/Scripts/Level/ColliderMap.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderMap : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.tag == "Player") {
+			Debug.Log ("Collided enter");
+			Destroy(other.gameObject);
+		}
+		//Destroy (wallc.gameObject);
+	}
+
+	/*void OnTriggerExit(Collider wallc){
+		Debug.Log ("Collided exit");
+		//Destroy (wallc.gameObject);
+	}*/
+}
