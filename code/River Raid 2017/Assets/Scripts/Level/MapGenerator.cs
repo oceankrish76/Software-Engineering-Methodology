b584ae74b4e4a1782705e99b430ffe1b152899d2
Rekalibrovka <kairbek.kuanysh@gmail.com>
2017-03-15 14:43:32 +0200

    Map modifications

--- code/River Raid 2017/Assets/Scripts/Level/MapGenerator.cs
+++ code/River Raid 2017/Assets/Scripts/Level/MapGenerator.cs
@@ -68,12 +68,9 @@ public class MapGenerator : MonoBehaviour {
 
 		for (int x = 0; x < width; x ++) {
 			for (int y = 0; y < height; y ++) {
-				if (x == 0 || x == width-1) {
+				if (x == 0 || x == width-1 || y <= 3) {
 					map[x,y] = 1;
 				}
-				else if (x > width / 10f && x < width - width / 10f && y > height - height / 20f) {
-					map[x,y] = 0;
-				}				
 				else {
 					map[x,y] = (pseudoRandom.Next(0,100) < randomFillPercent)? 1: 0;
 				}
@@ -94,19 +91,6 @@ public class MapGenerator : MonoBehaviour {
 			}
 		}
 	}
-	void SmoothUpdatedMap() {
-		for (int x = 0; x < width; x ++) {
-			for (int y = 0; y < height; y ++) {
-				int neighbourWallTiles = GetSurroundingWallCount(x,y);
-
-				if (neighbourWallTiles > 6)
-					map[x,y] = 1;
-				else if (neighbourWallTiles < 2)
-					map[x,y] = 0;
-
-			}
-		}
-	}
 
 	int GetSurroundingWallCount(int gridX, int gridY) {
 		int wallCount = 0;
@@ -122,36 +106,18 @@ public class MapGenerator : MonoBehaviour {
 				}
 			}
 		}
+
 		return wallCount;
 	}
-
-	bool InnerFieldCheck(int gridX, int gridY) {
-		for (int neighbourX = gridX - 1; neighbourX <= gridX + 1; neighbourX = neighbourX + 2) {
-			for (int neighbourY = gridY - 1; neighbourY <= gridY + 1; neighbourY = neighbourY + 2) {
-				if (neighbourX >= 0 && neighbourX < width && neighbourY >= 0 && neighbourY < height) {
-					//if (neighbourX != gridX || neighbourY != gridY) {
-						if (map [neighbourX, neighbourY] == 1)
-							return true;
-						else
-							continue;
-					//}
-				}
-			}
-		}
-		return false;
-	}
-
 
 	public void AddColliders (){
 		for (int x = 0; x < map.GetLength (0); x++) {
 			for (int y = 0; y < map.GetLength (1); y++) {
 				if (map[x,y] == 1) {
-					if (InnerFieldCheck(x, y)) {
-						BoxCollider boxCollider = gameObject.AddComponent<BoxCollider> ();
-						boxCollider.center = new Vector3 (x - width / 2, -1f, y - height / 2);
-						boxCollider.size = Vector3.one;
-						boxCollider.isTrigger = true;
-					}
+					BoxCollider boxCollider = gameObject.AddComponent<BoxCollider> ();
+					boxCollider.center = new Vector3 (x - width / 2, -1f, y - height / 2);
+					boxCollider.size = Vector3.one;
+					boxCollider.isTrigger = true;
 				}
 			}
 		}
@@ -171,10 +137,6 @@ public class MapGenerator : MonoBehaviour {
 
 		ClearMapColliders ();
 
-		for (int i = 0; i < 4; i++) {
-			SmoothUpdatedMap ();
-		}
-
 		int borderSize = 1;
 		int[,] borderedMap = new int[width + borderSize * 2,height];
 
@@ -184,7 +146,7 @@ public class MapGenerator : MonoBehaviour {
 					borderedMap[x,y] = map[x-borderSize,y];
 				}
 				else {
-					borderedMap[x,y] = 1;
+					borderedMap[x,y] =1;
 				}
 			}
 		}
@@ -204,8 +166,7 @@ public class MapGenerator : MonoBehaviour {
 					if (x == 0 || x == width - 1) {
 						map [x, y] = 1;
 					} else {
-						//int lfsr = (((((map [x, height / 2] + map [x, height / 2 - 1]) % 2) + map [x, height / 2 + 1]) % 2) + map [x, height / 2 - 2]) % 2;
-						int lfsr = (map[x,0] + map[x,1]) % 2;
+						int lfsr = (((((map [x, height / 2] + map [x, height / 2 - 1]) % 2) + map [x, height / 2 + 1]) % 2) + map [x, height / 2 - 2]) % 2;
 						map [x, y] = lfsr;
 					}
 				}
